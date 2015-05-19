e8c54e11f24645202286009826acc8ae7f35d078
hehe19900512 <870347271@qq.com>
2015-05-19 16:59:20 +0800

    Initial Commit

---
+++ ParallaxViewTests/ParallaxViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  ParallaxViewTests.swift
+//  ParallaxViewTests
+//
+//  Created by 张海迪 on 15/5/19.
+//  Copyright (c) 2015年 haidi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ParallaxViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
